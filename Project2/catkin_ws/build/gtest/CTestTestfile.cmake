22c6e165386108957c2b679619ec1d32f40bcd34
jboegeholz <boegeholz.joern@gmail.com>
2024-03-05 20:23:01 +0100

    add simpel_mover exe

--- Project2/catkin_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/jobo8505/udacity-nd-robotic-sw-eng/Project2/catkin_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
