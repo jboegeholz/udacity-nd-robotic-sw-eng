d9879e59cc2e7cdf9195569db2bfc9d4f6d71028
jboegeholz <boegeholz.joern@gmail.com>
2024-06-21 14:38:59 +0200

    rework

--- project2-go-chase-it/catkin_ws/src/ball_chaser/src/drive_bot.cpp
+++ project2-go-chase-it/catkin_ws/src/ball_chaser/src/drive_bot.cpp
@@ -26,17 +26,13 @@ int main(int argc, char** argv)
 {
     // Initialize a ROS node
     ros::init(argc, argv, "drive_bot");
-
     // Create a ROS NodeHandle object
     ros::NodeHandle n;
-
     // Inform ROS master that we will be publishing a message of type geometry_msgs::Twist on the robot actuation topic with a publishing queue size of 10
     motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
-
     ros::ServiceServer service = n.advertiseService("/ball_chaser/command_robot", handle_drive_request);
 
-    
     ros::spin();
 
     return 0;
